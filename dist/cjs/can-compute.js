163d4b88791dcd2def548dd04a0d1897b90a66b5
David Luecke <daff@neyeon.com>
2016-04-19 09:22:47 -0700

    3.0.0-pre.1

---
+++ dist/cjs/can-compute.js
@@ -0,0 +1,63 @@
+/*can-compute@3.0.0-pre.0#can-compute*/
+var canEvent = require('can-event');
+var canBatch = require('can-event/batch/batch');
+var Compute = require('./proto-compute.js');
+var CID = require('can-util/js/cid/cid');
+var COMPUTE = function (getterSetter, context, eventName, bindOnce) {
+    var internalCompute = new Compute(getterSetter, context, eventName, bindOnce);
+    var addEventListener = internalCompute.addEventListener;
+    var removeEventListener = internalCompute.removeEventListener;
+    var compute = function (val) {
+        if (arguments.length) {
+            return internalCompute.set(val);
+        }
+        return internalCompute.get();
+    };
+    var cid = CID(compute, 'compute');
+    var handlerKey = '__handler' + cid;
+    compute.bind = compute.addEventListener = function (ev, handler) {
+        var computeHandler = handler && handler[handlerKey];
+        if (handler && !computeHandler) {
+            computeHandler = handler[handlerKey] = function () {
+                handler.apply(compute, arguments);
+            };
+        }
+        return addEventListener.call(internalCompute, ev, computeHandler);
+    };
+    compute.unbind = compute.removeEventListener = function (ev, handler) {
+        var computeHandler = handler && handler[handlerKey];
+        if (computeHandler) {
+            delete handler[handlerKey];
+            return internalCompute.removeEventListener(ev, computeHandler);
+        }
+        return removeEventListener.apply(internalCompute, arguments);
+    };
+    compute.isComputed = internalCompute.isComputed;
+    compute.clone = function (ctx) {
+        if (typeof getterSetter === 'function') {
+            context = ctx;
+        }
+        return COMPUTE(getterSetter, context, ctx, bindOnce);
+    };
+    compute.computeInstance = internalCompute;
+    return compute;
+};
+COMPUTE.truthy = function (compute) {
+    return COMPUTE(function () {
+        var res = compute();
+        if (typeof res === 'function') {
+            res = res();
+        }
+        return !!res;
+    });
+};
+COMPUTE.async = function (initialValue, asyncComputer, context) {
+    return COMPUTE(initialValue, {
+        fn: asyncComputer,
+        context: context
+    });
+};
+COMPUTE.read = Compute.read;
+COMPUTE.set = Compute.set;
+COMPUTE.temporarilyBind = Compute.temporarilyBind;
+module.exports = exports = COMPUTE;
