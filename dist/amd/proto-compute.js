0a7cb7de56ff4156ea8e77c72d140bf61913a2c2
Justin Meyer <justinbmeyer@gmail.com>
2016-11-25 12:51:59 -0600

    3.0.4

---
+++ dist/amd/proto-compute.js
@@ -0,0 +1,286 @@
+/*can-compute@3.0.3#proto-compute*/
+define(function (require, exports, module) {
+    var Observation = require('can-observation');
+    var canEvent = require('can-event');
+    var eventLifecycle = require('can-event/lifecycle');
+    require('can-event/batch');
+    var observeReader = require('can-observation/reader');
+    var getObject = require('can-util/js/get');
+    var CID = require('can-util/js/cid');
+    var assign = require('can-util/js/assign');
+    var types = require('can-util/js/types');
+    var isEmptyObject = require('can-util/js/is-empty-object');
+    var Compute = function (getterSetter, context, eventName, bindOnce) {
+        CID(this, 'compute');
+        var args = [];
+        for (var i = 0, arglen = arguments.length; i < arglen; i++) {
+            args[i] = arguments[i];
+        }
+        var contextType = typeof args[1];
+        if (typeof args[0] === 'function') {
+            this._setupGetterSetterFn(args[0], args[1], args[2], args[3]);
+        } else if (args[1] !== undefined) {
+            if (contextType === 'string' || contextType === 'number') {
+                var isListLike = types.isListLike(args[0]);
+                if (types.isMapLike(args[0]) || isListLike) {
+                    var map = args[0];
+                    var propertyName = args[1];
+                    var mapGetterSetter = function (newValue) {
+                        if (arguments.length) {
+                            observeReader.set(map, propertyName, newValue);
+                        } else {
+                            if (isListLike) {
+                                observeReader.get(map, 'length');
+                            }
+                            return observeReader.get(map, '' + propertyName);
+                        }
+                    };
+                    this._setupGetterSetterFn(mapGetterSetter, args[1], args[2], args[3]);
+                } else {
+                    this._setupProperty(args[0], args[1], args[2]);
+                }
+            } else if (contextType === 'function') {
+                this._setupSetter(args[0], args[1], args[2]);
+            } else {
+                if (args[1] && args[1].fn) {
+                    this._setupAsyncCompute(args[0], args[1]);
+                } else {
+                    this._setupSettings(args[0], args[1]);
+                }
+            }
+        } else {
+            this._setupSimpleValue(args[0]);
+        }
+        this._args = args;
+        this._primaryDepth = 0;
+        this.isComputed = true;
+    };
+    var updateOnChange = function (compute, newValue, oldValue, batchNum) {
+        var valueChanged = newValue !== oldValue && !(newValue !== newValue && oldValue !== oldValue);
+        if (valueChanged) {
+            canEvent.dispatch.call(compute, {
+                type: 'change',
+                batchNum: batchNum
+            }, [
+                newValue,
+                oldValue
+            ]);
+        }
+    };
+    var setupComputeHandlers = function (compute, func, context) {
+        var observation = new Observation(func, context, compute);
+        compute.observation = observation;
+        return {
+            _on: function () {
+                observation.start();
+                compute.value = observation.value;
+                compute.hasDependencies = !isEmptyObject(observation.newObserved);
+            },
+            _off: function () {
+                observation.stop();
+            },
+            getDepth: function () {
+                return observation.getDepth();
+            }
+        };
+    };
+    assign(Compute.prototype, {
+        setPrimaryDepth: function (depth) {
+            this._primaryDepth = depth;
+        },
+        _setupGetterSetterFn: function (getterSetter, context, eventName) {
+            this._set = context ? getterSetter.bind(context) : getterSetter;
+            this._get = context ? getterSetter.bind(context) : getterSetter;
+            this._canObserve = eventName === false ? false : true;
+            var handlers = setupComputeHandlers(this, getterSetter, context || this);
+            assign(this, handlers);
+        },
+        _setupProperty: function (target, propertyName, eventName) {
+            var self = this, handler;
+            handler = function () {
+                self.updater(self._get(), self.value);
+            };
+            this._get = function () {
+                return getObject(target, propertyName);
+            };
+            this._set = function (value) {
+                var properties = propertyName.split('.'), leafPropertyName = properties.pop(), targetProperty = getObject(target, properties.join('.'));
+                targetProperty[leafPropertyName] = value;
+            };
+            this._on = function (update) {
+                canEvent.on.call(target, eventName || propertyName, handler);
+                this.value = this._get();
+            };
+            this._off = function () {
+                return canEvent.off.call(target, eventName || propertyName, handler);
+            };
+        },
+        _setupSetter: function (initialValue, setter, eventName) {
+            this.value = initialValue;
+            this._set = setter;
+            assign(this, eventName);
+        },
+        _setupSettings: function (initialValue, settings) {
+            this.value = initialValue;
+            this._set = settings.set || this._set;
+            this._get = settings.get || this._get;
+            if (!settings.__selfUpdater) {
+                var self = this, oldUpdater = this.updater;
+                this.updater = function () {
+                    oldUpdater.call(self, self._get(), self.value);
+                };
+            }
+            this._on = settings.on ? settings.on : this._on;
+            this._off = settings.off ? settings.off : this._off;
+        },
+        _setupAsyncCompute: function (initialValue, settings) {
+            var self = this;
+            var getter = settings.fn;
+            var bindings;
+            this.value = initialValue;
+            this._setUpdates = true;
+            this.lastSetValue = new Compute(initialValue);
+            this._set = function (newVal) {
+                if (newVal === self.lastSetValue.get()) {
+                    return this.value;
+                }
+                return self.lastSetValue.set(newVal);
+            };
+            this._get = function () {
+                return getter.call(settings.context, self.lastSetValue.get());
+            };
+            if (getter.length === 0) {
+                bindings = setupComputeHandlers(this, getter, settings.context);
+            } else if (getter.length === 1) {
+                bindings = setupComputeHandlers(this, function () {
+                    return getter.call(settings.context, self.lastSetValue.get());
+                }, settings);
+            } else {
+                var oldUpdater = this.updater, resolve = Observation.ignore(function (newVal) {
+                        oldUpdater.call(self, newVal, self.value);
+                    });
+                this.updater = function (newVal) {
+                    oldUpdater.call(self, newVal, self.value);
+                };
+                bindings = setupComputeHandlers(this, function () {
+                    var res = getter.call(settings.context, self.lastSetValue.get(), resolve);
+                    return res !== undefined ? res : this.value;
+                }, this);
+            }
+            assign(this, bindings);
+        },
+        _setupSimpleValue: function (initialValue) {
+            this.value = initialValue;
+        },
+        _eventSetup: Observation.ignore(function () {
+            this.bound = true;
+            this._on(this.updater);
+        }),
+        _eventTeardown: function () {
+            this._off(this.updater);
+            this.bound = false;
+        },
+        addEventListener: eventLifecycle.addAndSetup,
+        removeEventListener: eventLifecycle.removeAndTeardown,
+        clone: function (context) {
+            if (context && typeof this._args[0] === 'function') {
+                this._args[1] = context;
+            } else if (context) {
+                this._args[2] = context;
+            }
+            return new Compute(this._args[0], this._args[1], this._args[2], this._args[3]);
+        },
+        _on: function () {
+        },
+        _off: function () {
+        },
+        get: function () {
+            var recordingObservation = Observation.isRecording();
+            if (recordingObservation && this._canObserve !== false) {
+                Observation.add(this, 'change');
+                if (!this.bound) {
+                    Compute.temporarilyBind(this);
+                }
+            }
+            if (this.bound) {
+                if (this.observation) {
+                    return this.observation.get();
+                } else {
+                    return this.value;
+                }
+            } else {
+                return this._get();
+            }
+        },
+        _get: function () {
+            return this.value;
+        },
+        set: function (newVal) {
+            var old = this.value;
+            var setVal = this._set(newVal, old);
+            if (this._setUpdates) {
+                return this.value;
+            }
+            if (this.hasDependencies) {
+                return this._get();
+            }
+            this.updater(setVal === undefined ? this._get() : setVal, old);
+            return this.value;
+        },
+        _set: function (newVal) {
+            return this.value = newVal;
+        },
+        updater: function (newVal, oldVal, batchNum) {
+            this.value = newVal;
+            if (this.observation) {
+                this.observation.value = newVal;
+            }
+            updateOnChange(this, newVal, oldVal, batchNum);
+        },
+        toFunction: function () {
+            return this._computeFn.bind(this);
+        },
+        _computeFn: function (newVal) {
+            if (arguments.length) {
+                return this.set(newVal);
+            }
+            return this.get();
+        }
+    });
+    Compute.prototype.on = Compute.prototype.bind = Compute.prototype.addEventListener;
+    Compute.prototype.off = Compute.prototype.unbind = Compute.prototype.removeEventListener;
+    var k = function () {
+    };
+    var computes;
+    var unbindComputes = function () {
+        for (var i = 0, len = computes.length; i < len; i++) {
+            computes[i].removeEventListener('change', k);
+        }
+        computes = null;
+    };
+    Compute.temporarilyBind = function (compute) {
+        var computeInstance = compute.computeInstance || compute;
+        computeInstance.addEventListener('change', k);
+        if (!computes) {
+            computes = [];
+            setTimeout(unbindComputes, 10);
+        }
+        computes.push(computeInstance);
+    };
+    Compute.async = function (initialValue, asyncComputer, context) {
+        return new Compute(initialValue, {
+            fn: asyncComputer,
+            context: context
+        });
+    };
+    Compute.truthy = function (compute) {
+        return new Compute(function () {
+            var res = compute.get();
+            if (typeof res === 'function') {
+                res = res.get();
+            }
+            return !!res;
+        });
+    };
+    module.exports = exports = Compute;
+});
