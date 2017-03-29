f6cf9e2eabd4ae17b2ae9feebf08acc39b9e8d06
Justin Meyer <justinbmeyer@gmail.com>
2017-03-29 10:00:22 -0500

    3.1.0

---
+++ dist/amd/single-reference.js
@@ -0,0 +1,22 @@
+/*can-compute@3.1.0-pre.0#single-reference*/
+define(function (require, exports, module) {
+    (function (global) {
+        var canReflect = require('can-reflect/reflections/get-set');
+        var CID = require('can-cid');
+        var singleReference;
+        singleReference = {
+            set: function (obj, key, value) {
+                canReflect.set(obj, CID(key), value);
+            },
+            getAndDelete: function (obj, key) {
+                var cid = CID(key);
+                var value = canReflect.get(obj, cid);
+                canReflect.delete(obj, cid);
+                return value;
+            }
+        };
+        module.exports = singleReference;
+    }(function () {
+        return this;
+    }()));
+});
