8a89924b8c42d6a0733f66b23ad4567518c66781
Kevin Phillips <kevin@bitovi.com>
2017-06-22 21:10:36 -0500

    3.2.0

---
+++ dist/amd/can-compute.js
@@ -0,0 +1,84 @@
+/*can-compute@3.1.0-pre.17#can-compute*/
+define(function (require, exports, module) {
+    require('can-event');
+    require('can-event/batch');
+    var Compute = require('./proto-compute');
+    var CID = require('can-cid');
+    var namespace = require('can-namespace');
+    var singleReference = require('can-util/js/single-reference');
+    var canReflect = require('can-reflect/reflections/get-set');
+    var canSymbol = require('can-symbol');
+    var canOnValueSymbol = canSymbol.for('can.onValue'), canOffValueSymbol = canSymbol.for('can.offValue'), canGetValue = canSymbol.for('can.getValue'), canSetValue = canSymbol.for('can.setValue'), isValueLike = canSymbol.for('can.isValueLike'), isMapLike = canSymbol.for('can.isMapLike'), isListLike = canSymbol.for('can.isListLike'), isFunctionLike = canSymbol.for('can.isFunctionLike'), canValueHasDependencies = canSymbol.for('can.valueHasDependencies'), canGetValueDependencies = canSymbol.for('can.getValueDependencies');
+    var addEventListener = function (ev, handler) {
+        var compute = this;
+        var translationHandler;
+        if (handler) {
+            translationHandler = function () {
+                handler.apply(compute, arguments);
+            };
+            singleReference.set(handler, this, translationHandler);
+        }
+        return compute.computeInstance.addEventListener(ev, translationHandler);
+    };
+    var removeEventListener = function (ev, handler) {
+        return this.computeInstance.removeEventListener(ev, handler && singleReference.getAndDelete(handler, this));
+    };
+    var onValue = function (handler) {
+            return this.computeInstance[canOnValueSymbol](handler);
+        }, offValue = function (handler) {
+            return this.computeInstance[canOffValueSymbol](handler);
+        }, getValue = function () {
+            return this.computeInstance.get();
+        }, setValue = function (value) {
+            return this.computeInstance.set(value);
+        }, hasDependencies = function () {
+            return this.computeInstance.hasDependencies;
+        }, getDependencies = function () {
+            return this.computeInstance[canGetValueDependencies]();
+        };
+    var COMPUTE = function (getterSetter, context, eventName, bindOnce) {
+        function compute(val) {
+            if (arguments.length) {
+                return compute.computeInstance.set(val);
+            }
+            return compute.computeInstance.get();
+        }
+        var cid = CID(compute, 'compute');
+        compute.computeInstance = new Compute(getterSetter, context, eventName, bindOnce);
+        compute.handlerKey = '__handler' + cid;
+        compute.on = compute.bind = compute.addEventListener = addEventListener;
+        compute.off = compute.unbind = compute.removeEventListener = removeEventListener;
+        compute.isComputed = compute.computeInstance.isComputed;
+        compute.clone = function (ctx) {
+            if (typeof getterSetter === 'function') {
+                context = ctx;
+            }
+            return COMPUTE(getterSetter, context, ctx, bindOnce);
+        };
+        canReflect.set(compute, canOnValueSymbol, onValue);
+        canReflect.set(compute, canOffValueSymbol, offValue);
+        canReflect.set(compute, canGetValue, getValue);
+        canReflect.set(compute, canSetValue, setValue);
+        canReflect.set(compute, isValueLike, true);
+        canReflect.set(compute, isMapLike, false);
+        canReflect.set(compute, isListLike, false);
+        canReflect.set(compute, isFunctionLike, false);
+        canReflect.set(compute, canValueHasDependencies, hasDependencies);
+        canReflect.set(compute, canGetValueDependencies, getDependencies);
+        return compute;
+    };
+    COMPUTE.truthy = function (compute) {
+        return COMPUTE(function () {
+            var res = compute();
+            return !!res;
+        });
+    };
+    COMPUTE.async = function (initialValue, asyncComputer, context) {
+        return COMPUTE(initialValue, {
+            fn: asyncComputer,
+            context: context
+        });
+    };
+    COMPUTE.temporarilyBind = Compute.temporarilyBind;
+    module.exports = namespace.compute = COMPUTE;
+});
