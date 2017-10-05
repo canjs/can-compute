7209d27f97bf17fa314c24ca20910a524c6ef534
Juan Orozco <hi@juanorozco.com>
2017-10-05 13:31:04 -0400

    Improve behavior Unbind with no arguments
    Calling unbind with no arguments now removes all handlers for the compute.
    
    - Updated can-event version dependency
    - Added test

--- can-compute_test.js
+++ can-compute_test.js
@@ -6,6 +6,7 @@ var QUnit = require('steal-qunit');
 var canBatch = require('can-event/batch/');
 var Observation = require('can-observation');
 var domDispatch = require("can-util/dom/dispatch/dispatch");
+var isEmptyObject = require("can-util/js/is-empty-object/is-empty-object")
 var canSymbol = require("can-symbol");
 var canReflect = require("can-reflect");
 //require('./read_test');
@@ -667,4 +668,14 @@ QUnit.test("can-reflect setValue", function(){
 
 	canReflect.setValue(a, "A");
 	QUnit.equal(a(), "A", "compute");
+});
+
+QUnit.test("Calling .unbind() with no arguments should tear down all event handlers", function () {
+	var count = compute(0);
+  count.on('change', function() {
+		console.log('Count changed');
+	});
+	QUnit.equal(count.computeInstance.__bindEvents.change.length, 1, "Change event added");
+	count.unbind();
+	QUnit.equal(count.computeInstance.__bindEvents.change.length, 0, "All events for compute removed");
 });
