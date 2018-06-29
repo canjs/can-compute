6f4b0f3ca2e791038cc35b4b633bc16a1661b5c9
cherif BOUCHELAGHEM <cherifbouchelaghem@yahoo.fr>
2018-06-29 22:32:30 +0100

    fix how domEvent dispatch works on test

--- can-compute_test.js
+++ can-compute_test.js
@@ -2,13 +2,12 @@ require("./proto-compute_test");
 var compute = require('can-compute');
 var QUnit = require('steal-qunit');
 var ObservationRecorder = require("can-observation-recorder");
-var domEvents = require("can-dom-events");
 var canSymbol = require("can-symbol");
 var canReflect = require("can-reflect");
 var eventQueue = require("can-event-queue/map/map");
 var queues = require("can-queues");
+require("can-dom-events");
 
-var domDispatch = domEvents.dispatch;
 var metaSymbol = canSymbol.for("can.meta");
 
 QUnit.module('can/compute');
